97f8a291eba029de50f119f8ef95f6e6e8f62edb
Giuliana Gonzalez <giulianamichelleg@gmail.com>
2026-03-31 16:43:54 -0300

    add crud

--- lista-vetor-01/croud.js
+++ lista-vetor-01/croud.js
@@ -17,8 +17,8 @@ function mostrarMenu() {
     console.log("3- buscar por Id")
     console.log("4-atualizar alunos")
     console.log("5- remover alunos")
-    console.log("6-mostrar alunos reprovados")
-    console.log("7-mostrr alunos reprovados")
+    console.log("6-mostrar alunos aprovados")
+    console.log("7-mostrar alunos reprovados")
     console.log("0-sair")
     console.log("==================")
 
@@ -29,8 +29,144 @@ function mostrarMenu() {
             listaAluno();
         } else if (opcao === "3") {
             buscarAlunoPorId();
+        } else if (opcao === "4") {
+            atualizarAluno();
+        } else if (opcao === "5") {
+            removerAluno();
+        } else if (opcao === "6") {
+            AlunosAprovados();
+        } else if (opcao === "7") {
+            alunosReprovados();
         }
     })
+
+}
+
+function alunosReprovados() {
+    console.log("\nAlunos Reprovados")
+    console.log("\n===================")
+
+    let encontrou = false;
+
+    for (i = 0; i < alunos.length; i++) {
+        if (alunos[i].nota < 6) {
+
+            console.log("\nID: " + alunos[i].id);
+            console.log("\nNome: " + alunos[i].nome);
+            console.log("\nNota: " + alunos[i].nota);
+
+            encontrou = true
+        }
+
+    }
+
+    if (!encontrou) {
+        console.log("Nenhum aluno Reprovado")
+    }
+
+    mostrarMenu();
+
+}
+
+
+function AlunosAprovados() {
+    console.log("\nAlunos aprovados")
+    console.log("\n==================")
+
+    let encontrou = false;
+    for (i = 0; i < alunos.length; i++) {
+        if (alunos[i].nota >= 6) {
+
+            console.log("\nID: " + alunos[i].id);
+            console.log("\nNome: " + alunos[i].nome);
+            console.log("\nNota: " + alunos[i].nota);
+
+            encontrou = true
+        }
+
+    }
+
+    if (!encontrou) {
+        console.log("Nenhum aluno aprovado")
+    }
+
+
+    mostrarMenu();
+
+}
+
+
+function removerAluno() {
+    console.log("Remover aluno");
+
+    rl.question("Digite o ID do aluno para remover: ", (id) => {
+        id = Number(id);
+
+        for (let i = 0; i < alunos.length; i++) {
+            if (alunos[i].id === id) {
+                alunos.splice(i, 1);
+                console.log("Aluno removido com sucesso");
+                mostrarMenu();
+                return;
+            }
+
+        }
+        console.log("Aluno não encontrado");
+        mostrarMenu();
+
+
+
+
+    })
+}
+
+
+function atualizarAluno() {
+    console.log("Atualizar aluno")
+    rl.question("Digite o ID do aluno: ", (id) => {
+        id = Number(id);
+
+        let aluno = encontrarAlunoPorId(id);
+
+        if (aluno === null) {
+            console.log("Aluno não encontrado");
+            mostrarMenu();
+            return;
+        }
+
+        rl.question("Digite um novo nome: ", (novoNome) => {
+            rl.question("Digite a nova idade: ", (novaIdade) => {
+                rl.question("Digite a nova turma: ", (novaTurma) => {
+                    rl.question("Digite a nova nota: ", (novaNota) => {
+
+                        novaIdade = Number(novaIdade);
+                        novaNota = Number(novaNota);
+                        if (novoNome === "" || novaIdade === "" || novaTurma === "" || novaNota === "") {
+                            console.log("Todos os dados precisam ser preenchidos");
+                            mostrarMenu();
+                            return;
+                        }
+
+                        if (novaIdade < 0 || novaNota < 0 || novaNota > 10 || novaIdade > 100) {
+                            console.log("Idade ou nota inválida ");
+                            mostrarMenu();
+                            return;
+                        }
+                        aluno.nome = novoNome;
+                        aluno.idade = novaIdade;
+                        aluno.nota = novaNota;
+                        aluno.turma = novaTurma;
+
+                        console.log("Atualizado com sucesso");
+                        mostrarMenu();
+
+                    });
+                });
+            });
+        });
+
+    });
+
 
 }
 
@@ -118,7 +254,7 @@ function buscarAlunoPorId() {
 
     })
 }
-function encontrarAlunoPorId() {
+function encontrarAlunoPorId(id) {
     for (let i = 0; i < alunos.length; i++) {
         if (alunos[i].id === id) {
             return alunos[i];
